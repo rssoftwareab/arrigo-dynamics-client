e3b30bce1104ac87cd0acf034fef1be8e0f837e6
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-10-16 16:52:59 -0300

    Improving config to a future environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 export const config: any = {
   auth: {
-    account: "",
-    password: "exo",
-    username: "ReginSe",
+    account: process.env.ACCOUNT || "",
+    password: process.env.PASSWORD || "exo",
+    username: process.env.USERNAME || "ReginSe",
   },
-  url: "https://services.regin.se/ci/eos_beta/api/",
-  ws: "ws://172.18.149.35/eos_beta/api/graphql/ws",
+  url: process.env.URL || "https://services.regin.se/ci/eos_beta/api/",
+  ws: process.env.WS || "ws://172.18.149.35/eos_beta/api/graphql/ws",
 }
