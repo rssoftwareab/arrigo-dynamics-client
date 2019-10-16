d04e813f59f8f46f913e21dcd5d0b4e2a24249d4
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-10-16 16:47:14 -0300

    Adding the config File as an Information Concentrator

---
+++ src/config/config.ts
@@ -0,0 +1,9 @@
+export const config: any = {
+  auth: {
+    account: "",
+    password: "exo",
+    username: "ReginSe",
+  },
+  url: "https://services.regin.se/ci/eos_beta/api/",
+  ws: "ws://172.18.149.35/eos_beta/api/graphql/ws",
+}
