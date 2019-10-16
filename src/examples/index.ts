49746e3e055f96f6ab4bf4e80aeb40e3166b6ef3
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-10-16 16:49:34 -0300

    Refactoring index from examples folder

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,65 +1,16 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloClient } from "apollo-client";
-import { WebSocketLink } from "apollo-link-ws";
-import gql from "graphql-tag";
-import WebSocket from "ws";
+
 import { DynamicsClient } from "../client";
-import { OnDataPayload } from "../client/OnDataPayload"
-import { gqlquery, login } from "./APIhelper";
+import { OnDataPayload } from "../client/OnDataPayload";
+import { getFileContentByVariableUuid } from "./helpers/api/queries/getFileContentByVariableUuid"
+import { getFirstVariableByFolderId } from "./helpers/api/queries/getFirstVariableByFolderId"
+import { subscribeOnFileByVariableUuid } from "./helpers/api/subscriptions/subscribeOnFileByVariableUuid"
+import { apolloClient } from "./helpers/apolloClient"
 
 async function run() {
-  const authToken = await login("", "ReginSe", "exo");
-
-  console.log(authToken);
-  const res = await gqlquery(
-    `{
-      folder(id: "QXJlYV9B") {
-        name
-        ...on UserArea {
-          publicVariableList {
-            name
-            uid
-          }		
-        }
-      }
-    }`,
-    authToken
-  );
-
-  const pvl = res.data.folder.publicVariableList[1];
-  console.log("public variable lists", pvl);
-
-  const fileQuery = await gqlquery(
-    `{data(uid:"${pvl.uid}"){content}}`,
-    authToken
-  );
-  
-  const content = fileQuery.data.data.content;
-  console.log("filecontent", content);
-
-  const link = new WebSocketLink({
-    uri: "ws://172.18.149.35/eos_beta/api/graphql/ws",
-
-    options: {
-      reconnect: true
-    },
-    webSocketImpl: WebSocket
-  });
-
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link,
-  });
-
-  const subscriptionQuery = gql`
-  subscription {
-    data(uid:"${pvl.uid}")
-    {
-      path
-      type
-      value
-    }
-  }`;
+  const pvl = await getFirstVariableByFolderId("QXJlYV9B")
+  const content = await getFileContentByVariableUuid(pvl.uid)
+  const client = await apolloClient()
+  const subscriptionQuery = await subscribeOnFileByVariableUuid(pvl.uid)
 
   const dynamicsClient = new DynamicsClient();
   dynamicsClient.initialize({
