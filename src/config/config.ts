27e8ba39bd86e6ed41c4a380165d12b0a71e1380
Teodor Andersson <teodor.andersson@rssoftware.se>
2019-10-24 11:43:48 +0200

    changed environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 export const config: any = {
   auth: {
     account: process.env.ACCOUNT || "",
-    password: process.env.PASSWORD || "exo",
-    username: process.env.USERNAME || "ReginSe",
+    password: process.env.API_PASSWORD || "exo",
+    username: process.env.API_USERNAME || "ReginSe"
   },
   url: process.env.URL || "https://services.regin.se/ci/eos_beta/api/",
-  ws: process.env.WS || "ws://172.18.149.35/eos_beta/api/graphql/ws",
-}
+  ws: process.env.WS || "ws://172.18.149.35/eos_beta/api/graphql/ws"
+};
